a9dd6488059d53ea9733b2adc1fd18d8f020c737
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-03-25 10:19:28 +0100

    Removed unwanted folder

--- backEnd/public/js/bundle.js
+++ backEnd/public/js/bundle.js
@@ -12494,7 +12494,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61770" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63883" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
